bf8650fec89cc697b4af1923198dcb3b0389f4c9
Nahngom Zyegan Naam <nahngompansha@gmail.com>
2024-12-05 23:28:30 +0530

    Update BackgroundScrolling.cs

--- Assets/MyGameScripts/Scripts_GameMenu/BackgroundScrolling.cs
+++ Assets/MyGameScripts/Scripts_GameMenu/BackgroundScrolling.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BackgroundScrolling : MonoBehaviour
 {
-    public float scrollSpeed = 0.5f;  // Adjust this to control how fast the background moves
+    public float scrollSpeed = 0.5f;
     private Vector2 startPosition;
     private float textureHeight;
 
@@ -14,13 +14,10 @@ public class BackgroundScrolling : MonoBehaviour
 
     void Update()
     {
-        // Move the background down at the specified speed
         transform.position += Vector3.down * scrollSpeed * Time.deltaTime;
 
-        // Check if the background has moved off the bottom of the screen
         if (transform.position.y <= startPosition.y - textureHeight)
         {
-            // Reset its position to the start position to create a loop
             transform.position = new Vector2(startPosition.x, startPosition.y);
         }
     }
